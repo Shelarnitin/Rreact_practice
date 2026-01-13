4817d4278bb46736a5ff7dd0e97d744c9c470d56
Shelarnitin <shelarnitin399@gmail.com>
2026-01-13 20:55:25 +0530

    06_react

---
+++ 06_react/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useRef, useState } from "react";
+
+function Greeting() {
+  const nameRef = useRef(null);
+  const [name, setName] = useState("");
+
+  const showName = () => {
+    setName(nameRef.current.value);
+  };
+
+  return (
+    <>
+      <h1>Hello, {name}</h1>
+      <input ref={nameRef} type="text" />
+      <button onClick={showName}>Greet</button>
+    </>
+  );
+}
+
+export default Greeting;
