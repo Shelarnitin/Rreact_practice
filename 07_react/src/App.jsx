ced28b2e04283716b02f0cceaac3c3549d840317
Shelarnitin <shelarnitin399@gmail.com>
2026-01-18 01:21:47 +0530

    add & remove Numbers

--- 07_react/src/App.jsx
+++ 07_react/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+// function RemoveNumber() {
+//   const [numbers, setNumbers] = useState([1, 2, 3, 4]);
+
+//   const removeThree = () => {
+//     const updatedArray = numbers.filter(num => num !== 3);
+//     setNumbers(updatedArray);
+//   };
+
+//   return (
+//     <div>
+//       <h2>Numbers: {numbers.join(', ')}</h2>
+//       <button onClick={removeThree}>Remove 3</button>
+//     </div>
+//   );
+// }
+
+// export default RemoveNumber;
+
+function removeNumber () {
+  const [number, setNumbers] = useState([1,2,3,4])
+
+
+  const removeThree = () => {
+    const updateArray = number.filter(num => num !== 3)
+    setNumbers(updateArray)
+  }
+
+  const addThree = () => {
+    // const updateArray = number.filter > 2 ? num => num ===3 : number
+    setNumbers([...number,3])
+  }
+
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Numbers : {number.join(', ')}</p>
+      <button onClick={removeThree}>Remove</button>
+      <button onClick={addThree}>Add</button>
     </>
   )
 }
 
-export default App
+export default removeNumber
