fbeacfdee9717e2ab9f8a8198021e360c9ab4b35
Shelarnitin <shelarnitin399@gmail.com>
2026-01-18 01:35:22 +0530

    Remove & Add Number

--- 07_react/src/App.jsx
+++ 07_react/src/App.jsx
@@ -1,45 +1,28 @@
-import React, { useState } from 'react';
+// const { useState } = require("react");
+import { useState } from "react"
+// import React {useState} from "react"
 
+function PerformOnNumbers () {
+  const [number, setNumber] = useState([1,2,3,4])
 
-// function RemoveNumber() {
-//   const [numbers, setNumbers] = useState([1, 2, 3, 4]);
-
-//   const removeThree = () => {
-//     const updatedArray = numbers.filter(num => num !== 3);
-//     setNumbers(updatedArray);
-//   };
-
-//   return (
-//     <div>
-//       <h2>Numbers: {numbers.join(', ')}</h2>
-//       <button onClick={removeThree}>Remove 3</button>
-//     </div>
-//   );
-// }
-
-// export default RemoveNumber;
-
-function removeNumber () {
-  const [number, setNumbers] = useState([1,2,3,4])
-
-
-  const removeThree = () => {
-    const updateArray = number.filter(num => num !== 3)
-    setNumbers(updateArray)
+  const RemoveFour = () => {
+    const updateArray = number.filter(num => num !==4)
+    setNumber(updateArray)
   }
 
-  const addThree = () => {
-    // const updateArray = number.filter > 2 ? num => num ===3 : number
-    setNumbers([...number,3])
+  const AddFour = () => {
+    if(!number.includes(4)){
+      setNumber([...number, 4])
+    }
   }
 
   return(
     <>
-      <p>Numbers : {number.join(', ')}</p>
-      <button onClick={removeThree}>Remove</button>
-      <button onClick={addThree}>Add</button>
+      <h1>Numbers : {number.join(', ')}</h1>
+      <button onClick={RemoveFour}>Remove</button>
+      <button onClick={AddFour}>Add</button>
     </>
   )
 }
 
-export default removeNumber
+export default PerformOnNumbers
