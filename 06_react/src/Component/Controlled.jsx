3ae1790f7fc7ee17ec1aee0fa38b5f8e5dfcae17
Shelarnitin <shelarnitin399@gmail.com>
2026-01-17 12:43:30 +0530

    Controlled

--- 06_react/src/Component/Controlled.jsx
+++ 06_react/src/Component/Controlled.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function Controlled() {
-    const [name, setName] = useState('');
+function Controlled () {
+    const [name, setName] =useState('')
 
-    function handleSubmit (e){
+
+    function handleSubmit(e){
         e.preventDefault();
         alert(name)
     }
 
-  return (
-    <div>
-        <form onSubmit={handleSubmit}>
-            <input 
-            type="text"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            />
-            <button type='submit'>Submit</button>
-        </form>
-    </div>
-  )
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                />
+                <button type="submit">submit</button>
+            </form>
+        </>
+    )
+
+
 }
 
 export default Controlled
