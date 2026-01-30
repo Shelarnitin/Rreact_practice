ba0b8b0ca87b8bc4d032c1f460769cfd03a5d49b
Shelarnitin <shelarnitin399@gmail.com>
2026-01-30 10:35:16 +0530

    Robot.jsx

--- 08_react/src/Robot.jsx
+++ 08_react/src/Robot.jsx
@@ -1,5 +1,5 @@
-function RobotPart(props){
-    return <div>{props.PartName} is ready!</div>
+function RobotPart(props) {
+  return <div>{props.PartName} is ready!</div>;
 }
 
-export default RobotPart
+export default RobotPart;
